7c717c8be704b9e435c011994893e000b383d7c1
vsaxena12 <36344027+vsaxena12@users.noreply.github.com>
2025-06-03 07:05:20 -0500

    adding thymeleaf files

--- thymeleaf-demo/src/main/java/com/spring_boot/thymeleaf_demo/controller/HelloWorldController.java
+++ thymeleaf-demo/src/main/java/com/spring_boot/thymeleaf_demo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.spring_boot.thymeleaf_demo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +17,16 @@ public class HelloWorldController {
     // Process the HTML form
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld"; //Connects the view (helloworld.html) with the controller
+    }
+
+    @RequestMapping("/processFormV2")
+    public String processFormV2(HttpServletRequest request, Model model) {
+        String name = request.getParameter("studentName");
+        name = name.toUpperCase();
+
+        String result = "Hello " + name;
+        model.addAttribute("message", result);
         return "helloworld"; //Connects the view (helloworld.html) with the controller
     }
 
